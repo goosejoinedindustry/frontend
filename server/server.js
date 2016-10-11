953db638de0978c379c4d3bc004d2a1189156a68
Justin <justintime225@Justins-MacBook-Pro.local>
2016-10-11 15:13:58 -0700

    build(project): Set up initial webpack structure

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join('../' + __dirname, 'client')));
+
+const head = {company: 'Industry'};
+app.get('*', (req, res) => {
+  res.render('index', {head});
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Connected to port ${PORT}`);
+});
