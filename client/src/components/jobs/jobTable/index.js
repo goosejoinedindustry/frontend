64a8f604ba755046bb0d1effad5046c3b737089d
Justin <jnguyen@industry.co>
2016-10-20 10:56:50 -0700

    build: Add grid test and remove MUI from lower level components

--- client/src/components/jobs/jobTable/index.js
+++ client/src/components/jobs/jobTable/index.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { map } from 'lodash';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow } from 'material-ui/Table';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import JobTableRow from './jobTableRow';
+
 injectTapEventPlugin();
 
 export default class JobTable extends Component {
@@ -15,29 +15,27 @@ export default class JobTable extends Component {
       return <div />;
     }
     return (
-      <MuiThemeProvider>
-        <Table selectable>
-          <TableHeader
-            displaySelectAll={false}
-            displayRowCheckbox={false}
-            adjustForCheckbox={false}
-          >
-            <TableRow>
-              <TableHeaderColumn>Posted</TableHeaderColumn>
-              <TableHeaderColumn>Photo</TableHeaderColumn>
-              <TableHeaderColumn>Business</TableHeaderColumn>
-              <TableHeaderColumn>Position</TableHeaderColumn>
-              <TableHeaderColumn>Job Type</TableHeaderColumn>
-              <TableHeaderColumn>Location</TableHeaderColumn>
-              <TableHeaderColumn />
-              <TableHeaderColumn />
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {map(this.props.data.jobsList, result => <JobTableRow key={result.id} job={result} />)}
-          </TableBody>
-        </Table>
-      </MuiThemeProvider>
+      <Table selectable>
+        <TableHeader
+          displaySelectAll={false}
+          displayRowCheckbox={false}
+          adjustForCheckbox={false}
+        >
+          <TableRow>
+            <TableHeaderColumn>Posted</TableHeaderColumn>
+            <TableHeaderColumn>Photo</TableHeaderColumn>
+            <TableHeaderColumn>Business</TableHeaderColumn>
+            <TableHeaderColumn>Position</TableHeaderColumn>
+            <TableHeaderColumn>Job Type</TableHeaderColumn>
+            <TableHeaderColumn>Location</TableHeaderColumn>
+            <TableHeaderColumn />
+            <TableHeaderColumn />
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {map(this.props.data.jobsList, result => <JobTableRow key={result.id} job={result} />)}
+        </TableBody>
+      </Table>
     );
   }
 }
