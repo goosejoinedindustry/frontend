9319952989338fe24ef9e156a868c184a224d2ab
Justin <jnguyen@industry.co>
2016-10-20 12:15:23 -0700

    refactor: Refactor signupForm to material ui redux form

--- client/src/components/signup/signupForm.js
+++ client/src/components/signup/signupForm.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
+  <TextField hintText={label}
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    {...custom}
+  />
+);
+const renderSelectField = ({ input, label, meta: { touched, error }, children, ...custom }) => (
+  <SelectField
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    onChange={(event, index, value) => input.onChange(value)}
+    children={children}
+    {...custom}
+  />
+);
 
 class SignupForm extends Component {
   render() {
@@ -8,26 +30,36 @@ class SignupForm extends Component {
       <div>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email">Email</label>
-            <Field name="email" component="input" type="email" />
+            <Field name="firstName" component={renderTextField} label="First Name" />
           </div>
           <div>
-            <label htmlFor="zipcode">Zip Code</label>
-            <Field name="zipcode" component="input" type="number" />
+            <Field name="lastName" component={renderTextField} label="Last Name" />
           </div>
           <div>
-            <label htmlFor="firstName">First Name</label>
-            <Field name="firstName" component="input" type="text" />
+            <Field name="email" component={renderTextField} label="Email" />
           </div>
           <div>
-            <label htmlFor="lastName">First Name</label>
-            <Field name="lastName" component="input" type="text" />
+            <Field name="confirm email" component={renderTextField} label="Confirm Email" />
           </div>
           <div>
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <Field name="phoneNumber" component="input" type="number" />
+            <Field name="phoneNumber" component={renderTextField} label="Phone Number" />
           </div>
-          <button type="submit" disabled={submitting || pristine}>Sign Up</button>
+          <div>
+            <Field name="confirm phoneNumber" component={renderTextField} label="Confirm Phone Number" />
+          </div>
+          <div>
+            <Field name="zipcode" component={renderTextField} label="Zip Code" />
+          </div>
+          <div>
+            <Field name="age" component={renderSelectField} label="Select Age">
+              <MenuItem value={'ff0000'} primaryText="Young" />
+              <MenuItem value={'00ff00'} primaryText="Old" />
+              <MenuItem value={'0000ff'} primaryText="Dead" />
+            </Field>
+          </div>
+          <button type="submit" disabled={pristine || submitting}>Submit</button>
+          <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values
+          </button>
         </form>
       </div>
     );
